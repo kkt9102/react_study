0d5ebed203497badcfcb7a8e2b3f3bf0b43c848f
kkt9102 <kkt9102@gmail.com>
2021-07-25 16:05:23 +0900

    useState 여러개 사용하기

--- src/Say.js
+++ src/Say.js
@@ -5,13 +5,20 @@ const Say = () => {
     const onClickEnter = () => setMessage('안녕하세요!');
     const onClikcLeave = () => setMessage('안녕히 가세요!');
 
+    // 한 컴포넌트에서 useState 여러번 사용하기
+    const [color, setColor] = useState('black');
+
     return (
         <div>
             <button onClick={onClickEnter}>입장</button>
             <button onClick={onClikcLeave}>퇴장</button>
-            <h1>{message}</h1>
+            <h1 style={{ color }}>{message}</h1>
+            <button style={{ color: 'red'}} onClick={() => setColor('red')}>빨간색</button>
+            <button style={{ color: '#0000ff'}} onClick={() => setColor('#0000ff')}>파란색</button>
+            <button style={{ color: 'rgba(0,255,0,1)'}} onClick={() => setColor('rgba(0,255,0,1)')}>초록색</button>
         </div>
     );
 };
+
 
 export default Say;
